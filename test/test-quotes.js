10e20556cacfc08dae4130b0c7eb3aed1e801c2e
daviddinnison <david.g.dinnison@gmail.com>
2017-07-17 22:14:15 -0400

    basic tests added

--- test/test-quotes.js
+++ test/test-quotes.js
@@ -14,7 +14,7 @@ chai.use(chaiHttp);
 
 // this function deletes the entire database.
 // we'll call it in an `afterEach` block below
-// to ensure  ata from one test does not stick
+// to ensure data from one test does not stick
 // around for next one
 function tearDownDb() {
   return new Promise((resolve, reject) => {
@@ -42,7 +42,7 @@ function seedQuotesData() {
   return Quotes.insertMany(seedData);
 }
 
-describe('blog posts API resource', function () {
+describe('Quotes', function () {
 
   before(function () {
     return runServer(TEST_DATABASE_URL);
@@ -62,13 +62,68 @@ describe('blog posts API resource', function () {
     return closeServer();
   });
 
-  describe('* endpoint', function () {
-    it('should be true', function () {
-      // return Quotes
-      //   .then(res => {
-      //     res.should.have.status(404);
-      return 5 ===5;
-  
+
+  it('should list all quotes on GET', function() {
+    return chai.request(app)
+      .get('/quotes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.at.least(1);
+        const expectedKeys = ['source', 'quote'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  it('should create a new item on POST', function() {
+    const newItem = {quote: 'Oh, behave!', source: {firstName: 'Austin', lastName: 'Powers'}};
+    return chai.request(app)
+      .post('/quotes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'quote', 'source');
+        res.body.id.should.not.be.null;
+        //fail from previous examples (node-shopping-list-inegration-tests)
+        //res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  it('should update PUT items', function() {
+    const updateData = {
+      quote: 'The name is Bond, lol',
+      source: {firstName: 'James', lastName: 'Bond'}
+    };
+    return chai.request(app)
+    .get('/quotes')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+      return chai.request(app)
+        .put(`/quotes/${updateData.id}`)
+        .send(updateData)
+    })
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
     });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/quotes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/quotes/${res.body[0].id}`)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
   });
 });
