07274f3a2f2687a99839fd034358a73684c2f87f
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-18 16:02:53 -0400

    edited spacing

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
   global.DATABASE_URL ||
   'mongodb://localhost/quotes';
-  
+
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
   'mongodb://localhost/test-quotes');
